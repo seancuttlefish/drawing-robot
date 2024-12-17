95cd0ade16a72a4999db426e1ba1fe5568fb94a7
Sean Clark <seanc@interactdigitalarts.uk>
2024-12-17 16:50:30 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,15 +9,6 @@ function Left (duration: number) {
     minibit.rotatems(mbRobotDirection.Left, 20, duration)
     basic.showNumber(ID)
 }
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        Left(timeunit)
-    } else if (receivedNumber == 2) {
-        Right(timeunit)
-    } else if (receivedNumber == 3) {
-        Forward(timeunit)
-    }
-})
 function Forward (duration: number) {
     basic.showLeds(`
         . . . . .
@@ -29,18 +20,6 @@ function Forward (duration: number) {
     minibit.goms(mbDirection.Forward, 20, duration)
     basic.showNumber(ID)
 }
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-    Right(1)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(3)
-    Forward(1)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
-    Left(1)
-})
 function Right (duration: number) {
     basic.showLeds(`
         . # . . .
@@ -52,12 +31,14 @@ function Right (duration: number) {
     minibit.rotatems(mbRobotDirection.Right, 20, duration)
     basic.showNumber(ID)
 }
-let timeunit = 0
 let ID = 0
 ID = 1
-timeunit = 375
+let timeunit = 375
 radio.setGroup(ID)
 basic.showNumber(ID)
 basic.forever(function () {
-	
+    Left(300)
+    Forward(300)
+    Right(600)
+    Forward(300)
 })
