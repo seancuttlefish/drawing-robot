a4ab9afd817e06627176fef361f4dc0d07caf0a9
Sean Clark <seanc@interactdigitalarts.uk>
2025-01-06 16:02:00 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,54 +1,49 @@
 function Left (degrees: number) {
     basic.showArrow(ArrowNames.West)
-    minibit.rotatems(mbRobotDirection.Left, 20, degrees)
+    minibit.rotatems(mbRobotDirection.Left, speed, degrees)
     basic.showNumber(ID)
 }
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        Left(300)
+        Left(100)
     } else if (receivedNumber == 2) {
-        Right(300)
+        Right(100)
     } else if (receivedNumber == 3) {
         Forward(300)
+    } else if (receivedNumber == 4) {
+        Backward(100)
+    } else if (receivedNumber == 5) {
+        Stop()
     }
 })
 function Forward (distance: number) {
     basic.showArrow(ArrowNames.North)
-    minibit.goms(mbDirection.Forward, 20, distance)
+    minibit.goms(mbDirection.Forward, speed, distance)
     basic.showNumber(ID)
 }
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(3)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
-})
 function Right (degrees: number) {
     basic.showArrow(ArrowNames.East)
-    minibit.rotatems(mbRobotDirection.Right, 20, degrees)
+    minibit.rotatems(mbRobotDirection.Right, speed, degrees)
     basic.showNumber(ID)
 }
 function Stop () {
     minibit.stop(mbStopMode.Brake)
-    status = 0
+    busy = 0
+    basic.showNumber(ID)
 }
 function Backward (distance: number) {
     basic.showArrow(ArrowNames.South)
-    minibit.goms(mbDirection.Reverse, 20, distance)
+    minibit.goms(mbDirection.Reverse, speed, distance)
     basic.showNumber(ID)
 }
-let status = 0
+let speed = 0
+let busy = 0
 let ID = 0
 ID = 1
-status = 1
+busy = 0
 radio.setGroup(ID)
+speed = 70
 basic.showNumber(ID)
 basic.forever(function () {
-    Left(300)
-    Forward(300)
-    Right(300)
-    Backward(300)
+	
 })
